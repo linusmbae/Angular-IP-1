6493a05fbc5b603ee48d64be32eec65d7786edf0
Linus Mutugi <linusmutugi5178@gmail.com>
2020-03-29 16:14:45 +0300

    toggle complete

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+  showQuoteDetails:boolean;
+  constructor(public authorName:string, public userName:string, public quote:string)
+  {
+    this.showQuoteDetails=false;
+  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[]=[
+    new Quote("linus","lucy","this is my quote")
+  ]
+  toggleDetails(index)
+   {
+    this.quotes[index].showQuoteDetails = !this.quotes[index].showQuoteDetails;
+   }
   constructor() { }
 
   ngOnInit(): void {
